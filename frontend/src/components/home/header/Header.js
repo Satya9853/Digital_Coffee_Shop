ba8bc954ac28998100248564a2bb03be6d468350
satya bhusan <1902106@gmail.com>
2023-01-13 20:01:42 +0530

    Updated home design

--- frontend/src/components/home/header/Header.js
+++ frontend/src/components/home/header/Header.js
@@ -8,24 +8,25 @@ const Header = () => {
   return (
     <header>
       <div className="header_left">
-        <div className="circle">
-          <Link to="/" className="header_logo">
-            <Logo />
-          </Link>
-        </div>
+      <Link to="/" className="auth_link hover">
+          Home
+        </Link>
+        <Link to="/menu" className="auth_link hover">
+          Menu
+        </Link>
+        <Link to="/auth" className="auth_link hover">
+          Sing in
+        </Link>
       </div>
       <div className="header_middle">
         <div className="logo_name">Brew Coffee</div>
       </div>
       <div className="header_right">
-        <Link to="/" className="auth_link hover1">
-          Home
+        <Link to="/about" className="auth_link hover">
+          About us
         </Link>
-        <Link to="/menu" className="auth_link hover1">
-          Menu
-        </Link>
-        <Link to="/auth" className="auth_link hover1">
-          Sing in
+        <Link to="/contact" className="auth_link hover">
+          Contact us
         </Link>
         <Link to="/profile" className="profile_link ">
           <img src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg" alt="profile" />
--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -10,7 +10,20 @@ const Home = () => {
 
   return (
     <Fragment>
+    <div className="coffee-beans-image1"></div>
+    <div className="coffee-beans-image2"></div>
       <div className="home">
+      <div className="left-home">
+        <div className="left-home-text">
+          <span>WE BELIEVE</span>
+          <br></br>
+          <span>A  CUP  OF COFFEE</span>
+          <br></br>
+          <span>IS ONE OF THE SIMPLEST</span>
+          <br></br>
+          <span>PLEASURES IN LIFE</span>
+        </div>
+      </div>
         {showHistory && coffeeHistory && (
           <HistoryCard coffeeHistory={coffeeHistory} setCoffeeHistory={setCoffeeHistory} setShowHistory={setShowHistory} />
         )}
